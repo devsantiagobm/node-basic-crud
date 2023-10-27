9049100c7df3b07b93d1ab95ba22bab19e83d61e
Santiago Barrera Muñoz <96841115+devsantiagobm@users.noreply.github.com>
2023-10-27 15:45:11 -0500

    First commit

---
+++ Client.js
@@ -0,0 +1,28 @@
+import { ObjectId } from "mongodb"
+
+export class Client{
+    constructor(database){
+        this.collection = database.collection("clientes")
+    }
+
+    async find(){
+        return await this.collection.find({}).toArray()
+    }
+
+    async save(client){
+        await this.collection.insertOne(client)
+    }
+    
+    async delete(id){
+        await this.collection.deleteOne({_id: new ObjectId(id)})
+    }
+    
+    async update(id, client){
+        await this.collection.updateOne({_id: new ObjectId(id)}, {$set: client})
+    }
+
+    async findById(id){
+        return await this.collection.findOne({_id: new ObjectId(id)})
+    }
+    
+}
---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express"
+import clientRouter from "./src/routes/client.route.js"
+import cors from "cors"
+
+const app = express()
+
+app.use(express.json())
+app.use(cors({
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }))
+
+ 
+
+
+app.use("/clients", clientRouter)
+
+app.listen(8000, () => {
+    console.log("Servidor corriendo...");
+})
